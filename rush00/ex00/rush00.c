7eb020c2a30368514119bd82399582d133100325
Miguel Angel Rivera Notararigo <ntrrgx@gmail.com>
2019-07-29 10:31:31 -0700

    Add rush00

---
+++ rush00/ex00/rush00.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: migriver <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/14 18:19:45 by migriver          #+#    #+#             */
+/*   Updated: 2019/07/14 20:09:21 by migriver         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+int		ft_is_corner(int x, int y, int h, int v)
+{
+	if ((v == 0 && h == 0) || (v == 0 &&  h == x - 1))
+		return (1);
+	else if ((h == 0 && v == y - 1) || (h == x - 1 && v  == y - 1))
+		return (1);
+	return (0);
+}
+
+void	rush(int x, int y)
+{
+	int v;
+	int h;
+
+	v = 0;
+	while (v < y)
+	{
+		h = 0;
+		while (h < x)
+		{
+			if (ft_is_corner(x, y, h, v) == 1)
+				ft_putchar('o');
+			else if (v == 0 || v == y - 1)
+				ft_putchar('-');
+			else if (h == 0 || h == x - 1)
+				ft_putchar('|');
+			else 
+				ft_putchar(' ');
+			h++;	
+		}
+		if (x > 0 && y > 0)
+			ft_putchar('\n');
+		v++;
+	}
+	return ;
+}
